05ab20f8fe7331dd926f6d185efa23f42e8810f7
Lauren Musante and nikhil batta <lauren@>
2019-08-20 09:59:01 -0700

    Add alert if user laeves a field blank or enters a non-numerical input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
+function clear(){
+  var countTo = 0
+  var countBy= 0
+}
 $(document).ready(function(){
   $("#counter").submit(function(event){
     event.preventDefault();
     var countTo = parseInt($("#countTo").val());
     var countBy = parseInt($("#countBy").val());
     console.log(countTo)
-    for (var index=0; index<countTo; index+=countBy){
-    var counter =  index + countBy
-$("#output1").append("<li>" + counter + "</li>")
+    if(!countTo || !countBy){
+      alert("please input fields")
     }
-
+    else{
+      for (var index=0; index<=countTo; index+=countBy){
+      var counter =  index + countBy
+  $("#output1").append("<li>" + counter + "</li>")
+    }
+    }
   });
 });
